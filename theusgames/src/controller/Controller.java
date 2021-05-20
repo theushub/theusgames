bcd8be7abad50bd4c4a7250918515ec3f2436b79
theushub <53832627+theushub@users.noreply.github.com>
2021-05-20 11:34:00 -0300

    criamos conexão com o banco de dados

--- theusgames/src/controller/Controller.java
+++ theusgames/src/controller/Controller.java
@@ -7,27 +7,38 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import model.DAO;
+import model.JavaBeans;
+
 /**
  * Servlet implementation class Controller
  */
-@WebServlet(urlPatterns = {"/Controller", "/main"})
+@WebServlet(urlPatterns = { "/Controller", "/main" })
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Controller() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	// criar objetos para acessar a camada model
+	JavaBeans javabeans = new JavaBeans();
+	DAO dao = new DAO();
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public Controller() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// teste de conexão
+		dao.testarConexao();
+		
 	}
 
 }
--- theusgames/src/model/DAO.java
+++ theusgames/src/model/DAO.java
@@ -1,10 +1,66 @@
 package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 /**
  * camada model do modelo mvc
+ * 
  * @author matheus cesarini
  *
  */
 
 public class DAO {
+	/* conexão com o banco de dados */
+	// parâmetros de conexão
+	private String driver = "com.mysql.cj.jdbc.Driver";
+	private String url = "jdbc:mysql://10.26.49.100:3306/dbgames?useTimezone=true&serverTimezone=UTC";
+	private String user = "matheus";
+	private String password = "Senac@123456";
 
+	/**
+	 * conexao com o banco de dados
+	 * 
+	 * @return con
+	 */
+	private Connection conectar() {
+		// criar um objeto para conexão com o banco
+		Connection con = null;
+		// tratamento de exceções
+		try {
+			// execução do driver
+			Class.forName(driver);
+			// esta linha executa a conexão
+			con = DriverManager.getConnection(url, user, password);
+			return con;
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
+		}
+	}
+
+	/**
+	 * teste de conexao
+	 */
+	public void testarConexao() {
+		try {
+			//estabelecer a conexão (método conectar() "abrir a geladeira")
+			Connection con = conectar();
+			//escrever no console
+			System.out.println("teste de conexão");
+			System.out.println(con);
+			//finalizar a conexão ("fechar a porta da geladeira")
+			con.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	/* CRUD CREATE */
+
+	/* CRUD READ */
+
+	/* CRUD UPDATE */
+
+	/* CRUD DELETE */
 }
